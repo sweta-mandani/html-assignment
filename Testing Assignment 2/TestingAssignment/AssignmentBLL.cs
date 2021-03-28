908f9995d6c7e17250f424e7b96cff767fa77195
sweta-mandani <swetamandani2000@gmail.com>
2021-03-28 01:37:15 -0400

    'testing'

---
+++ Testing Assignment 2/TestingAssignment/AssignmentBLL.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace TestingAssignment
+{
+    public static class AssignmentBLL
+    {
+
+        // 1. lowercase characters converted to uppercase.
+        public static String AddUpperCase(this String input)
+        {
+            StringBuilder str = new StringBuilder(input);
+            int ln = str.Length;
+
+            for (int i = 0; i < ln; i++)
+            {
+                if (str[i] >= 'a' && str[i] <= 'z')
+                    str[i] = (char)(str[i] - 32);
+            }
+            return str.ToString();
+        }
+
+        //2.uppercase characters converted to     lowercase and vice versa.
+        public static String AddLowerCase(this String input)
+        {
+            StringBuilder str = new StringBuilder(input);
+            int ln = str.Length;
+
+            for (int i = 0; i < ln; i++)
+            {
+                if (str[i] >= 'A' && str[i] <= 'Z')
+                    str[i] = (char)(str[i] + 32);
+            }
+            return str.ToString();
+        }
+        
+        //3.string to title case 
+        public static string TitleCase(this string str)
+        {
+            str = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(str);
+            return str;
+        }
+
+       
+
+
+        //4.lower case or not
+
+        public static bool CheckLowerCase(this String str)
+        {
+            int ln = str.Length;
+
+            for (int i = 0; i < ln; i++)
+            {
+                if (str[i] >= 'A' && str[i] <= 'Z')
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+
+
+        // 5.first character have upper case and the rest lower case.
+        public static string FirstUpperLetter(this string str)
+        {
+            if (str == null)
+                return null;
+
+            if (str.Length > 1)
+                return char.ToUpper(str[0]) + str.Substring(1);
+
+            return str.ToUpper();
+        }
+
+
+        //6.upper case or not
+        public static bool CheckUpperCase(this String str)
+        {
+            int ln = str.Length;
+
+            for (int i = 0; i < ln; i++)
+            {
+                if (str[i] >= 'a' && str[i] <= 'z')
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+       
+        //7.valid numeric value or not.
+        public static bool NumberValidation(this String input)
+        {
+            int n;
+            bool isNumeric = int.TryParse(input, out n);
+            return isNumeric;
+        }
+
+        //8.remove the last character from given the string
+        public static String LastCharacterRemove(this String str)
+        {
+            if (str == null)
+                return null;
+            else
+                return str.Substring(0, str.Length - 1);
+        }
+
+        //9-word count from an input string.
+        public static int WordCount(this String input)
+        {
+            int ln = input.Length;
+            String[] words = input.Split(' ');
+
+            return words.Length;
+        }
+
+        //10. input string to integer.
+
+        public static int StringToInt(this String input)
+        {
+            int x = 0;
+
+            Int32.TryParse(input, out x);
+            return x;
+        }
+    }
+}
---
+++ Testing Assignment 2/TestingAssignment_Test/UnitTest1.cs
@@ -0,0 +1,152 @@
+using TestingAssignment;
+using System;
+using Xunit;
+
+namespace TestingAssignment_Test
+{
+    public class UnitTest1
+    {
+
+       
+        //1
+        [Fact]
+        public void Test_AddUpperCase()
+        {
+            //Arrange
+            var input = "sahil";
+            var expectedValue = "SAHIL";
+            // Act
+            var result = input.AddUpperCase();
+            //Assert
+            Assert.Equal(expectedValue, result);
+        }
+
+        //2
+        [Fact]
+        public void Test_AddLowerCase()
+        {
+            //Arrange
+            var input = "SAHIL";
+            var expectedValue = "sahil";
+            // Act
+            var result = input.AddLowerCase();
+            //Assert
+            Assert.Equal(expectedValue, result);
+        }
+
+        //3
+        [Fact]
+        public void Task_TitleCase()
+        {
+            //Arrange
+
+            var input = "tcs digital";
+            var expectedString = "Tcs Digital";
+
+            //Act
+            var result = input.TitleCase();
+
+
+            //Assert
+            Assert.Equal(expectedString, result);
+        }
+
+
+        //4
+
+        [Fact]
+        public void Test_CheckLowerCase()
+        {
+            //Arrange
+            var input = "do or die";
+            var expectedValue = true;
+            // Act
+            var result = input.CheckLowerCase();
+            //Assert
+            Assert.Equal(expectedValue, result);
+        }
+
+        //5
+
+        [Fact]
+        public void Test_FirstUpperLetter()
+        {
+            //Arrange
+            var input = "sweta";
+            var expectedValue = "Sweta";
+            // Act
+            var result = input.FirstUpperLetter();
+            //Assert
+            Assert.Equal(expectedValue, result);
+        }
+
+
+        //6
+        [Fact]
+        public void Test_CheckUpperCase()
+        {
+            //Arrange
+            var input = "SWETA";
+            var expectedValue = true;
+            // Act
+            var result = input.CheckUpperCase();
+            //Assert
+            Assert.Equal(expectedValue, result);
+        }
+
+        //7
+        [Fact]
+        public void Test_NumberValidation()
+        {
+            //Arrange
+            var input = "123";
+            var expectedValue = true;
+            // Act
+            var result = input.NumberValidation();
+            //Assert
+            Assert.Equal(expectedValue, result);
+        }
+
+        //8
+
+        [Fact]
+        public void Test_LastCharacterRemove()
+        {
+            //Arrange
+            var input = "sweta";
+            var expectedValue = "swet";
+            // Act
+            var result = input.LastCharacterRemove();
+            //Assert
+            Assert.Equal(expectedValue, result);
+        }
+
+        //9
+        [Fact]
+        public void Test_WordCount()
+        {
+            //Arrange
+            var input = "sahil";
+            var expectedValue = 1;
+            // Act
+            var result = input.WordCount();
+            //Assert
+            Assert.Equal(expectedValue, result);
+        }
+
+
+        //10
+
+        [Fact]
+        public void Test_StringToInt()
+        {
+            //Arrange
+            var input = "123";
+            var expectedValue = 123;
+            // Act
+            var result = input.StringToInt();
+            //Assert
+            Assert.Equal(expectedValue, result);
+        }
+    }
+}
